7853a3663358e9907a258b266305740f049afa31
Krisha8179 <krishnarao1440@gmail.com>
2023-06-16 16:50:21 +0530

    updated cart

--- app.js
+++ app.js
@@ -21,11 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('648b4ea25e39a1976ddb3b5f')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
-  next();
 });
 
 app.use('/admin', adminRoutes);
