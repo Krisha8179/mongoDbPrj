e4129485043d3fd2f9375e218d044e84517f46c0
Krisha8179 <krishnarao1440@gmail.com>
2023-06-17 14:14:28 +0530

    all updated with mongoose

--- models/user.js
+++ models/user.js
@@ -50,6 +50,12 @@ userSchema.methods.removeFromCart = function(productId) {
     this.cart.items = updatedCartItems;
     return this.save();
 }
+
+userSchema.methods.clearCart = function() {
+    this.cart = {items: []};
+    return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema);
 
 // const getDb = require('../util/database').getDb
